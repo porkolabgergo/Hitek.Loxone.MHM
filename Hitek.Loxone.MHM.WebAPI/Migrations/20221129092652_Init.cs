45bcd17b7c6b4a244c2ac100aea19187248d1121
porkolabgergo <pork.gergo@gmail.com>
2022-11-29 23:39:07 +0100

    SQL

---
+++ Hitek.Loxone.MHM.WebAPI/Migrations/20221129092652_Init.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hitek.Loxone.MHM.WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeID = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Vorname = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Nachname = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
---
+++ Hitek.Loxone.MHM.WebAPI/Migrations/LogRecord/20221129092742_Init.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hitek.Loxone.MHM.WebAPI.Migrations.LogRecord
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    EmployeeID = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Vorname = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Nachname = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.EmployeeID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LogRecords",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    LogType = table.Column<int>(type: "int", nullable: false),
+                    EmployeeID = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LogRecords", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LogRecords_Employee_EmployeeID",
+                        column: x => x.EmployeeID,
+                        principalTable: "Employee",
+                        principalColumn: "EmployeeID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LogRecords_EmployeeID",
+                table: "LogRecords",
+                column: "EmployeeID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LogRecords");
+
+            migrationBuilder.DropTable(
+                name: "Employee");
+        }
+    }
+}
